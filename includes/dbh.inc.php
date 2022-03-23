cc36e90e77957f1cfc45260a08215f3528f36afd
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-23 15:31:04 +0100

    added heroku db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,26 @@
 <?php
 
-$serverName = "localhost:3309";
+/* $serverName = "localhost:3309";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "kundenverwaltungssystem";
 // mySQLi is up to date 
-$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName); */
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+?>
