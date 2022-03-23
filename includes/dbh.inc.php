8d56a33f6f88f4d5aec2ecb97e39c59d4857f033
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-23 17:36:16 +0100

    added db connection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
-
 /* $serverName = "localhost:3309";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "kundenverwaltungssystem";
 // mySQLi is up to date 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName); */
+
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -15,12 +15,9 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-/* //create connection
-$conn = new mysqli("eu-cdbr-west-02.cleardb.net", "b7830bd54b1c85", "8ff41e9b", "heroku_1f33d5651ee0787");
-$conn = mysqli_connect() */
 ?>
 
 <?php
