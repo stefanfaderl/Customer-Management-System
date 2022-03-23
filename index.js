7719677523c6d07652294d2708ceaf093a75e2d6
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-23 17:56:12 +0100

    added new logo

--- index.js
+++ index.js
@@ -1,96 +1,84 @@
 for (let i = 0; i < document.links.length; i++) {
-    let current = 0; 
-    if (document.links[i].href === document.URL && document.links[i].id != 'padding') {
-        current = i;
-        document.links[current].id = 'current';
-    }
-    if (document.links[i].href === document.URL && document.links[i].id == 'padding') {
-        current = i;
-        document.links[current].id = 'paddingActive';
-    }
+  let current = 0;
+  if (
+    document.links[i].href === document.URL &&
+    document.links[i].id != "padding"
+  ) {
+    current = i;
+    document.links[current].id = "current";
+  }
+  if (
+    document.links[i].href === document.URL &&
+    document.links[i].id == "padding"
+  ) {
+    current = i;
+    document.links[current].id = "paddingActive";
+  }
 }
 
 function sendToHomescreen() {
-    location.href = "dashboard.php"; // extend 
+  location.href = "index.php"; // extend
 }
 
 function toggleCheckbox(e) {
-    let checkboxes = document.getElementsByClassName('inputCheckbox');
-    console.log(e);
-    if (e.checked == true) {
-        for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = true;
-        }
-    } else {
-        for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = false;
-        }
+  let checkboxes = document.getElementsByClassName("inputCheckbox");
+  console.log(e);
+  if (e.checked == true) {
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = true;
     }
+  } else {
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = false;
+    }
+  }
 }
 
 function deleteContact() {
-    let selectedEntry = $('[name="checkbox"]:checked').data('id');
-    console.log(selectedEntry);  //'input:checkbox'
-    // if ($('[name="checkbox"]:checked').length > 0) {
-    $('#deleteForm').html('<input type="hidden" name="customerID" value="' + selectedEntry + '">');
-    // let result = confirm("Are you sure to delete selected customers?");
-    // if (result) {
-    //     console.log(this);
-    //     return true;
+  let selectedEntry = $('[name="checkbox"]:checked').data("id");
+  console.log(selectedEntry); //'input:checkbox'
+  // if ($('[name="checkbox"]:checked').length > 0) {
+  $("#deleteForm").html(
+    '<input type="hidden" name="customerID" value="' + selectedEntry + '">'
+  );
+  // let result = confirm("Are you sure to delete selected customers?");
+  // if (result) {
+  //     console.log(this);
+  //     return true;
 
-    // } else {
-    //     return false;
-    // }
-    // } else {
+  // } else {
+  //     return false;
+  // }
+  // } else {
 
-    //     alert('Select at least 1 record to delete!');
-    //     return false;
-    // }
+  //     alert('Select at least 1 record to delete!');
+  //     return false;
+  // }
 }
 
 function deleteContact() {
-    $('input.inputCheckbox:checked').each(function () {
-        let selectedEntry = $(this).data('id');
-        $('#deleteForm').append('<input type="hidden" name="customerID' + selectedEntry + '" value="' + selectedEntry + '">');
-    });
+  $("input.inputCheckbox:checked").each(function () {
+    let selectedEntry = $(this).data("id");
+    $("#deleteForm").append(
+      '<input type="hidden" name="customerID' +
+        selectedEntry +
+        '" value="' +
+        selectedEntry +
+        '">'
+    );
+  });
 }
 
 function editContact() {
-    let selectedEntry = $('input.inputCheckbox:checked').data('id');
-    $('#editForm').html('<input type="hidden" name="customerID" value="' + selectedEntry + '">');
-
+  let selectedEntry = $("input.inputCheckbox:checked").data("id");
+  $("#editForm").html(
+    '<input type="hidden" name="customerID" value="' + selectedEntry + '">'
+  );
 }
 
 function hideForm() {
-    $(".inputDashboard").hide();
+  $(".inputDashboard").hide();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 deleteContact:
