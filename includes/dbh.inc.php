912e45626016a72252eba590ff8cf6b0125afaba
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-23 16:52:33 +0100

    c

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -12,7 +12,7 @@ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["eu-cdbr-west-02.cleardb.net"];
 $cleardb_username = $cleardb_url["b7830bd54b1c85"];
 $cleardb_password = $cleardb_url["8ff41e9b"];
-$cleardb_db = substr($cleardb_url["mysql://b7830bd54b1c85:8ff41e9b@eu-cdbr-west-02.cleardb.net/heroku_1f33d5651ee0787?reconnect=true"], 1);
+$cleardb_db = substr($cleardb_url["heroku_1f33d5651ee0787"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
