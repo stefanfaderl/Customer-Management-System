8a2bd53fd7ddf3475f285127afe180af80f6fc9d
stuefi94 <btc.stefanfaderl@gmail.com>
2022-03-23 15:51:05 +0100

    c

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -8,7 +8,7 @@ $dbName = "kundenverwaltungssystem";
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName); */
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("heroku_1f33d5651ee0787"));
 $cleardb_server = $cleardb_url["eu-cdbr-west-02.cleardb.net"];
 $cleardb_username = $cleardb_url["b7830bd54b1c85"];
 $cleardb_password = $cleardb_url["8ff41e9b"];
@@ -17,7 +17,6 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
